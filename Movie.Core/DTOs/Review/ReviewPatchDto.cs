536253cbede6ac4197db0e4b89188eb75c8318c7
wach78 <dev@wach.nu>
2026-06-26 11:10:26 +0200

    small fixes

--- Movie.Core/DTOs/Review/ReviewPatchDto.cs
+++ Movie.Core/DTOs/Review/ReviewPatchDto.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using Movie.Core.Constants;
+
 namespace Movie.Core.DTOs.Review;
 
 public sealed class ReviewPatchDto
 {
+    [StringLength(ReviewValidationConstants.ReviewerNameMaxLength)]
     public string? ReviewerName { get; set; }
 
+    [StringLength(ReviewValidationConstants.CommentMaxLength)]
     public string? Comment { get; set; }
 
+    [Range(
+        ReviewValidationConstants.MinimumRating,
+        ReviewValidationConstants.MaximumRating
+    )]
     public int? Rating { get; set; }
 }
