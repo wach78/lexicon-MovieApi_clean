e472ddb2db945349c761c5d24e741da740038e08
wach78 <dev@wach.nu>
2026-06-26 10:15:07 +0200

    added versions

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Asp.Versioning.OpenApi;
 using Microsoft.EntityFrameworkCore;
 using Movie.Core.DomainContracts;
 using Movie.Data;
@@ -15,45 +18,105 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var connectionString = builder.Configuration.GetConnectionString("MovieApiContext") ?? throw new InvalidOperationException("Connection string 'MovieApiContext' not found.");
+        WebApplicationBuilder builder =
+            WebApplication.CreateBuilder(args);
 
-        builder.Services.AddDbContext<MovieApiContext>(options => options.UseSqlServer(connectionString));
+        string connectionString =
+            builder.Configuration.GetConnectionString(
+                "MovieApiContext"
+            )
+            ?? throw new InvalidOperationException(
+                "Connection string 'MovieApiContext' not found."
+            );
 
-        builder.Services.AddScoped<IMovieApiContext>(serviceProvider => serviceProvider.GetRequiredService<MovieApiContext>());
+        builder.Services.AddDbContext<MovieApiContext>(
+            options => options.UseSqlServer(connectionString)
+        );
+
+        builder.Services.AddScoped<IMovieApiContext>(
+            serviceProvider =>
+                serviceProvider.GetRequiredService<MovieApiContext>()
+        );
+
+        // Services
         builder.Services.AddScoped<IMovieService, MovieService>();
         builder.Services.AddScoped<IActorService, ActorService>();
         builder.Services.AddScoped<IReviewService, ReviewService>();
         builder.Services.AddScoped<IGenreService, GenreService>();
         builder.Services.AddScoped<IReportsService, ReportsService>();
-
         builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
+        // Repositories and Unit of Work
         builder.Services.AddScoped<IMovieRepository, MovieRepository>();
         builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
         builder.Services.AddScoped<IActorRepository, ActorRepository>();
-        builder.Services.AddScoped<IMovieDetailsRepository, MovieDetailsRepository>();
+        builder.Services.AddScoped<IMovieDetailsRepository,MovieDetailsRepository>();
         builder.Services.AddScoped<IGenreRepository, GenreRepository>();
         builder.Services.AddScoped<IReportRepository, ReportRepository>();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        // Add services to the container.
-
+        // Controllers
         builder.Services
             .AddControllers()
-            .AddApplicationPart(typeof(MoviesController).Assembly);
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+            .AddApplicationPart(
+                typeof(MoviesController).Assembly
+            );
 
-        var app = builder.Build();
+        // API versioning and versioned OpenAPI documents
+        builder.Services
+            .AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion =
+                    new ApiVersion(1, 0);
+
+                options.ReportApiVersions = true;
+
+                options.ApiVersionReader =
+                    new UrlSegmentApiVersionReader();
+            })
+            .AddMvc()
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+
+                options.SubstituteApiVersionInUrl = true;
+            })
+            .AddOpenApi(options =>
+            {
+                options.Document.AddScalarTransformers();
+            });
+
+        WebApplication app = builder.Build();
 
         app.SeedData();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
-            app.MapScalarApiReference();
+            app.MapOpenApi()
+                .WithDocumentPerVersion();
+
+            app.MapScalarApiReference(options =>
+            {
+                IReadOnlyList<ApiVersionDescription> descriptions =
+                    app.DescribeApiVersions();
+
+                foreach (
+                    ApiVersionDescription description in descriptions
+                )
+                {
+                    bool isDefault = string.Equals(
+                        description.GroupName,
+                        "v1",
+                        StringComparison.OrdinalIgnoreCase
+                    );
+
+                    options.AddDocument(
+                        description.GroupName,
+                        description.GroupName,
+                        isDefault: isDefault
+                    );
+                }
+            });
         }
 
         app.UseHttpsRedirection();
